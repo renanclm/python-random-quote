dcc6f459a60b457300c1c09e0c9b452e63baad82
Renan Monteiro Chaves Leite <renan.leite@petrobras.com.br>
2020-12-11 02:07:53 -0300

    Last commit

--- get-quote.py
+++ get-quote.py
@@ -7,10 +7,14 @@ def primary():
   quotes = f.readlines()
   f.close()
   
-  last = len(quotes)-1
-  rnd = random.randint(0,last)
-  
-  print(quotes[rnd])
+  size = len(quotes)
+  rnd1 = random.randint(0,size-1)
+  rnd2 = random.randint(0,size-1)
+  if rnd1 == rnd2: 
+    rnd2 = (rnd2+3)%size
+
+  print(quotes[rnd1].strip())
+  print(quotes[rnd2].strip())
 
 if __name__== "__main__":
   primary()
